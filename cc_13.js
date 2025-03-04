2f6d3ad1af26cd28ff0f9d6b77aee6b9f832c2a5
Jbarb25 <barboza205@usf.edu>
2025-03-04 17:13:35 -0500

    Task 4 - Employee Card Removal with Event Bubbling
    From the removal button step in task 2, we will add a stop.Propagation operation that will make it so that when the the remove employee button is clicked, it will not bubble to the container and print "employee section clicked." This will only remove the employee. Adding an event listener to the new employee section, when there is a click on an employee card, the message "employee section clicked" will be logged.

--- cc_13.js
+++ cc_13.js
@@ -17,8 +17,10 @@ employeeCard.innerHTML = `
     //create remove button that removed the employee card when pressed
 const removeEmployeeButton = employeeCard.querySelector('.remove-employee'); //pull the remove employee button by its ID remove-employee
 
-removeEmployeeButton.addEventListener('click', () => {
-    emContainerByID.removeChild(employeeCard);
+removeEmployeeButton.addEventListener('click', (event) => {
+      //Task 4: The following two lines involve task 4
+    event.stopPropagation(); //clicking the removal button will not print the "employee section clicked" message
+    emContainerByID.removeChild(employeeCard); //remove the selected employee card
 });
 
 emContainerByID.appendChild(employeeCard);} //add element employee card to the dashboard and the employeeContainer 
@@ -37,3 +39,13 @@ arrayOfNodes.forEach( item => {  //for every item in the array of nodes, do the 
         item.style.color = "#800000";  //change the text color to maroon in the printed html file
         item.style.fontFamily = 'Courier'}); //change the text font to courier in the printed html file
 
+
+//Task 4: Implementing Remove of Employee Cards with Event Bubbling
+   //Refer to task 4 for removing a parent employee card using removeChild  
+   //Refer to task 4 for stoping propagation building off of the following
+    const employeeSec = document.getElementById('employeeContainer'); //pull the parent container for the customer section by the id
+    
+    employeeSec.addEventListener('click', () => { 
+        console.log(`Employee Section clicked`);});  //add an event listner so that when the employee section is clicked, it logs the message
+
+        
